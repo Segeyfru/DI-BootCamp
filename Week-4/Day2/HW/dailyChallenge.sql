a6367291809af67ce12264a9cf1d2bf7375103d0
sergeyfru <sn.fruman@gmail.com>
2024-03-18 16:14:55 +0200

    add

---
+++ Week-4/Day2/HW/dailyChallenge.sql
@@ -0,0 +1,32 @@
+
+-- (5,'Pawan'),
+-- (6,'Sharlee'),
+-- (7,'Krish'),
+-- (NULL,'Avtaar');
+
+
+-- Q1. What will be the OUTPUT of the following statement?
+-- SELECT COUNT (*) FROM FirstTab AS ft WHERE ft.id NOT IN (SELECT id FROM SecondTab WHERE id IS NULL)
+
+-- count = 0
+
+-- Q2. What will be the OUTPUT of the following statement?
+
+-- 		SELECT COUNT(*) 
+-- 		FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+
+-- count = 2
+
+-- Q3. What will be the OUTPUT of the following statement?
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
+
+-- count = 0
+
+-- Q4. What will be the OUTPUT of the following statement?
+
+-- SELECT COUNT(*) 
+-- FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
+
+-- count =2
