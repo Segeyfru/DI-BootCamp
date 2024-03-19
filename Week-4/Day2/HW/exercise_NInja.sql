74d5b4800cbf712532d0d7ef9bc71b8ff8e2d4aa
sergeyfru <sn.fruman@gmail.com>
2024-03-19 21:41:33 +0200

    add

--- Week-4/Day2/HW/exercise_NInja.sql
+++ Week-4/Day2/HW/exercise_NInja.sql
@@ -1,1 +1,14 @@
-SElECT * FROM (SELECT * FROM customer ORDER BY customer_id DESC LIMIT 2)AS last_two ORDER BY customer_id ASC
+-- 		TASK 1
+-- SElECT * FROM (SELECT * FROM customers ORDER BY id DESC LIMIT 2)AS last_two ORDER BY id ASC;
+
+-- 		TASK 2
+
+-- DELETE FROM purchases 
+-- USING customers 
+-- WHERE purchases.customer_id = customers.id
+-- AND customers.first_name = 'Scott';
+
+-- SELECT * FROM purchases;
+
+-- 		TASK 3
+-- 		TASK 4
