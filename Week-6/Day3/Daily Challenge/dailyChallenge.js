b3a1a88c73b7b3bd0db17549635ed700275ec1f2
sergeyfru <sn.fruman@gmail.com>
2024-04-03 13:41:00 +0200

    exercisesXp

--- Week-6/Day3/Daily Challenge/dailyChallenge.js
+++ Week-6/Day3/Daily Challenge/dailyChallenge.js
@@ -11,20 +11,38 @@ class Video{
 
 let newVideo = new Video('Funy cat','user1154',135);
 newVideo.watch();
+
 let newVideo2 = new Video('Dog and mirror', 'Vasya Pupkin', 94)
 console.log(newVideo);
+
+
+// option 1
 let arr = [
     ['video1', 'video2', 'video3', 'video4', 'video5'],
     ['user321','user9485','user744','user5621','user1054'],
     [434, 646,854,120,65]
 ]
 let arrOfVideos = []
-// for (let i in arr){
-//     for(let i)
-// }
 
 for(let i =0; i< arr[0].length; i++){
     let video = new Video(arr[0][i], arr[1][i],arr[2][i])
-arrOfVideos.push(video)
+    arrOfVideos.push(video)
 }
 console.log(arrOfVideos);
+
+// the better method
+let arr1 = [
+    {title:'video10',uploader:'user12',time:34},
+    {title:'video654',uploader:'user654',time:340},
+    {title:'video8123',uploader:'user654',time:8123},
+    {title:'video345',uploader:'user345',time:345},
+    {title:'video610',uploader:'user610',time:65},
+]
+let arrOfVideos1 = []
+
+arr1.forEach((item) => {
+    let {title,uploader,time} = item
+    arrOfVideos1.push(new Video(title,uploader,time))
+})
+
+console.log(arrOfVideos1);
---
+++ Week-6/Day4/script.js
@@ -0,0 +1,56 @@
+// function findIntersection(arr){
+//     let arr1 = arr[0].split(',');
+//     let arr2 = arr[1].split(',');
+//     let ret = arr1.filter((item) => {
+//         return arr2.includes(item)
+//     });
+//     return ret
+// }
+// console.log(findIntersection( ["1,2,5,6,7", "2,5,7,8,15"]));
+
+
+// function findIntersection1(arr){
+//     let num = []
+//     let arr1 = arr[0].split(',');
+//     let arr2 = arr[1].split(',');
+//     for (let index = 0; index < arr1.length; index++) {
+//         const element1 = arr1[index];
+//         console.log(element1);
+//         for (let index = 0; index < arr2.length; index++) {
+//             const element2 = arr2[index];
+//             console.log(element2);
+//             if(element1 === element2){
+//                 num.push(element2)
+//             }
+            
+//         }
+        
+//     }
+//     return num;
+// }
+
+// console.log(findIntersection1( ["1,2,5,6,7", "2,5,7,8,15"]));
+
+
+// /**************************************************************************************************************************************************/
+
+/** JSON */
+
+let posts1 = `[
+    {
+      "userId": 5,
+      "id": 41,
+      "title": "non est facere",
+      "body": "molestias id nostrumexcepturi molestiae dolore omnis repellendus quaerat saepeconsectetur iste quaerat tenetur asperiores accusamus ex utnam quidem est ducimus sunt debitis saepe"
+    }
+  ]`;
+let posts = `[
+    {
+      "userId": 4,
+      "id": 31,
+      "title": "ullam ut quidem id aut vel consequuntur",
+      "body": "debitis eius sed quibusdam non quis consectetur vitae impedit ut qui consequatur sed aut in quidem sit nostrum et maiores adipisci atque quaerat voluptatem adipisci repudiandae"
+    }
+  ]`;
+let arr = JSON.parse(posts1);
+console.log(arr);
