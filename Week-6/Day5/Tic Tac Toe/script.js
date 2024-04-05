9fbd8cb991a14114d94fe3cbfbf83cf63b756a59
sergeyfru <sn.fruman@gmail.com>
2024-04-05 23:14:35 +0200

    add

--- Week-6/Day5/Tic Tac Toe/script.js
+++ Week-6/Day5/Tic Tac Toe/script.js
@@ -2,7 +2,11 @@ let user = 'X';
 let comp = 'O';
 const winer = document.getElementById('winer')
 const pad = document.getElementById('pad')
-
+const winCombos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [6, 4, 2],
+];
 
 const board = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
 function userChar(e){
@@ -47,20 +51,203 @@ function computerTurn(){
         console.log('no one!');
         boolean = false
     }
+    
+    
+    if(boolean){
+        console.log('going to win');
+        boolean = checkForWinWith2Matches();
+    }
+    
+    if (boolean){
+        console.log('going to block');
+        boolean = checkForBlockwith2Matches();
+    }
+    
+    if(boolean){
+        console.log('going to win with one match');
+        boolean = checkForWinWithOneMatch();
+        
+    }
+    if (boolean){
+        console.log('going to block with one match');
+        boolean = checkForBlockWithOneMatch();
+    }
+    
     while(boolean){
+        console.log('going to random');
         let compnum = Math.floor(Math.random() * 9)
         if(board[compnum] === -1){
             board[compnum] = comp;
             changeButton(compnum,comp)
             break
         }
-    }
-    
+    }   
+
     if (checkWiner()){
         showWiner(checkWiner())
         return
     }
 }
+
+function checkForWinWith2Matches(){
+    console.log(`Coincidence -> 2`);
+    for (const arr of winCombos) {  /** Checking win position */
+        console.log('Checking win position in ' + arr);
+        let [a,b,c] = arr;
+        let count = 0;
+        let num = -1;
+        if(board[a] === comp){
+            count++
+        }else if(board[a] === -1){
+            num = a;
+        }
+        if(board[b] === comp){
+            count++
+        }else if(board[b] === -1){
+            num = b;
+        }
+        if(board[c] === comp){
+            count++
+        }else if(board[c] === -1){
+            num = c;
+        }
+        console.log(board[a] + ' / ' + board[b] + ' / ' + board[c]);
+        console.log('counter = ' + count);
+        console.log('NUm = ' + num);
+        if(count === 2 && num >= 0){
+            board[num] = comp;
+            changeButton(num,comp)
+            if (checkWiner()){
+                showWiner(checkWiner())
+                return false
+            }
+            return false
+        }
+        console.log('not finded');
+    }
+    console.log('going to block');
+    return true
+}
+
+function checkForWinWithOneMatch(){
+    console.log(`Coincidence -> 1`);
+    for (const arr of winCombos) {  /** Checking win position */
+        console.log('Checking win position in ' + arr);
+        let [a,b,c] = arr;
+        let count = 0;
+        let num = -1;
+        if(board[a] !== user && board[b] !== user && board[c] !== user){
+            if(board[a] === comp){
+                count++
+            }else if(board[a] === -1){
+                num = a;
+            }
+            if(board[b] === comp){
+                count++
+            }else if(board[b] === -1){
+                num = b;
+            }
+            if(board[c] === comp){
+                count++
+            }else if(board[c] === -1){
+                num = c;
+            }
+            console.log(board[a] + ' / ' + board[b] + ' / ' + board[c]);
+            console.log('counter = ' + count);
+            console.log('NUm = ' + num);
+            if(count === 1 && num >= 0){
+                board[num] = comp;
+                changeButton(num,comp)
+                if (checkWiner()){
+                    showWiner(checkWiner())
+                    return false
+                }
+                return false
+            }
+        }        
+        console.log('not finded');
+    }
+    console.log('going to block');
+    return true
+}
+
+function checkForBlockwith2Matches(){/** Checking block position */
+    for (const arr of winCombos) { 
+        console.log('Checking block position in ' + arr);
+        let [a,b,c] = arr;
+        let count = 0;
+        let num = -1;
+        if(board[a] === user){
+            count++
+        }else if(board[a] === -1){
+            num = a;
+        }
+        if(board[b] === user){
+            count++
+        }else if(board[b] === -1){
+            num = b;
+        }
+        if(board[c] === user){
+            count++
+        }else if(board[c] === -1){
+            num = c;
+        }
+        console.log(board[a] + ' / ' + board[b] + ' / ' + board[c]);
+        console.log('counter = ' + count);
+        console.log('NUm = ' + num);
+        if(count === 2 && num >= 0){
+            board[num] = comp;
+            changeButton(num,comp)
+            if (checkWiner()){
+                showWiner(checkWiner())
+                return false
+            }
+            return false
+        }
+        console.log('not finded');
+    }
+    return true
+}
+
+function checkForBlockWithOneMatch(){/** Checking block position */
+    for (const arr of winCombos) { 
+        console.log('Checking block position in ' + arr);
+        let [a,b,c] = arr;
+        let count = 0;
+        let num = -1;
+        if(board[a] !== comp && board[b] !== comp && board[c] !== comp){
+            if(board[a] === user){
+                count++
+            }else if(board[a] === -1){
+                num = a;
+            };
+            if(board[b] === user){
+                count++
+            }else if(board[b] === -1){
+                num = b;
+            };
+            if(board[c] === user){
+                count++
+            }else if(board[c] === -1){
+                num = c;
+            };
+            console.log(board[a] + ' / ' + board[b] + ' / ' + board[c]);
+            console.log('counter = ' + count);
+            console.log('NUm = ' + num);
+            if(count === 1 && num >= 0){
+                board[num] = comp;
+                changeButton(num,comp)
+                if (checkWiner()){
+                    showWiner(checkWiner())
+                    return false
+                };
+                return false
+            };
+        };
+        console.log('not finded');
+    };
+    return true
+};
 
 function userPlay(element,index){
     if(board[index] !== -1){
@@ -71,6 +258,7 @@ function userPlay(element,index){
 
     changeButton(index,user)
     if (checkWiner()){
+        showWiner(checkWiner())
         return
     }else{computerTurn()}
     
@@ -102,7 +290,7 @@ function checkWiner() {
         }
     }
     if(!board.includes(-1)){
-        showWiner('no')
+        return 1
     }
     return 0;
 }
@@ -121,6 +309,7 @@ function restart(e){
 }
 
 function showWiner(winerChar){
+    console.log(board);
     pad.classList.add('hidden')
     document.getElementById('winer').style.display = 'block'
     if(winerChar === user ){
