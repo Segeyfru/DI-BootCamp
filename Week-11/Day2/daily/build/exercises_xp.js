98d232248790130bbbe6aa134904f9ce0967f061
Sergey <sn.fruman@gmail.com>
2024-05-22 15:03:55 +0300

    exercises 2,3,4,5

--- Week-11/Day2/daily/build/exercises_xp.js
+++ Week-11/Day2/daily/build/exercises_xp.js
@@ -35,5 +35,62 @@ console.log(person.lastName);
 console.log(person.getFullName());
 console.log(person._age);
 console.log(person.address);
-console.log(person.address = 'Israel');
+person.address = 'Israel';
 console.log(person.address);
+class Sedan {
+    constructor(make, model, numberOfDoors) {
+        this.make = make;
+        this.model = model;
+        this.numberOfDoors = numberOfDoors;
+    }
+    start() {
+        return `It is ${this.model}, and it has ${this.numberOfDoors} doors`;
+    }
+}
+const newSedan = new Sedan('BYD', 'Dolphin', 5);
+console.log(newSedan.start());
+// Exercise 3
+const combineObjects = (obj1, obj2) => {
+    const newObj = Object.assign(Object.assign({}, obj1), obj2);
+    return newObj;
+};
+console.log(combineObjects({ name: "John" }, { age: 23 }));
+// Exercise 4
+class Stack {
+    constructor(elements = []) {
+        this.elements = elements;
+    }
+    push(element) {
+        this.elements.push(element);
+    }
+    pop() {
+        const topElement = this.elements.pop();
+        return topElement;
+    }
+    isEmpty() {
+        if (this.elements.length > 0) {
+            return `The length of your stack is ${this.elements.length}`;
+        }
+        return 'Stack is empty';
+    }
+}
+const stack1 = new Stack(['ee', 'www', 'fff']);
+console.log(stack1.pop());
+console.log(stack1);
+console.log(stack1.isEmpty());
+console.log(stack1.push('e'));
+console.log(stack1);
+// Exercise 5
+function filterArray(arr, predicate) {
+    const newArray = [];
+    for (const element of arr) {
+        if (predicate(element)) {
+            newArray.push(element);
+        }
+    }
+    return newArray;
+}
+const numbers = [1, 2, 3, 4, 5];
+const isEven = (n) => n % 2 === 0;
+const evenNumbers = filterArray(numbers, isEven);
+console.log(evenNumbers);
--- Week-11/Day2/daily/src/exercises_xp.ts
+++ Week-11/Day2/daily/src/exercises_xp.ts
@@ -36,9 +36,86 @@ console.log(person.lastName);
 console.log(person.getFullName());
 console.log(person._age);
 console.log(person.address);
-console.log(person.address = 'Israel');
+person.address = 'Israel';
 console.log(person.address);
 
 
 // Exercise 2
 
+interface Vehicle {
+    make: string,
+    model: string,
+    start(): string
+}
+interface Car extends Vehicle {
+    numberOfDoors: number
+}
+
+class Sedan implements Car {
+
+    constructor(public make: string, public model: string, public numberOfDoors: number) { }
+
+    start(): string {
+        return `It is ${this.model}, and it has ${this.numberOfDoors} doors`
+    }
+}
+
+const newSedan = new Sedan('BYD', 'Dolphin', 5)
+
+console.log(newSedan.start())
+
+// Exercise 3
+
+const combineObjects = <T, U>(obj1: T, obj2: U): T & U => {
+    const newObj = { ...obj1, ...obj2 }
+    return newObj
+}
+
+console.log(combineObjects({ name: "John" }, { age: 23 }));
+
+// Exercise 4
+
+class Stack<T> {
+    constructor(public elements: T[] = []) { }
+
+    push(element: T) {
+        this.elements.push(element)
+    }
+
+    pop(): T | undefined {
+        const topElement = this.elements.pop()
+        return topElement
+    }
+    isEmpty() {
+        if (this.elements.length > 0) {
+            return `The length of your stack is ${this.elements.length}`
+        }
+        return 'Stack is empty'
+    }
+}
+const stack1 = new Stack(['ee', 'www', 'fff'])
+console.log(stack1.pop());
+console.log(stack1);
+console.log(stack1.isEmpty());
+console.log(stack1.push('e'));
+console.log(stack1);
+
+
+// Exercise 5
+
+function filterArray<T>(arr: T[], predicate: (element: T) => boolean): T[] {
+
+    const newArray: T[] = []
+    for (const element of arr) {
+        if (predicate(element)) {
+            newArray.push(element)
+        }
+    }
+    return newArray
+}
+
+const numbers = [1, 2, 3, 4, 5];
+const isEven = (n: number) => n % 2 === 0;
+
+const evenNumbers = filterArray(numbers, isEven);
+console.log(evenNumbers);
